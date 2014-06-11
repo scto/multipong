bcef80f5ae9371f49d37c85d8488071778a7a9ca
Johannes Sjölund <j.sjolund@gmail.com>
2014-06-12 01:55:31 +0200

    Added a letter roller class

---
+++ core/src/multipong/screens/LetterRoller.java
@@ -0,0 +1,43 @@
+package multipong.screens;
+
+public class LetterRoller {
+
+	public String[] letterList = { "a", "b", "c", "d", "e", "f", "g", "h", "i",
+			"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+			"x", "y", "z", "<-" };
+
+	private int selected = 0;
+	private int last = letterList.length - 1;
+
+	private String letters;
+
+	public void down() {
+		if (selected == last) {
+			selected = 0;
+		} else {
+			selected++;
+		}
+	}
+
+	public void up() {
+		if (selected == 0) {
+			selected = last;
+		} else {
+			selected--;
+		}
+	}
+
+	public void enter() {
+		letters += letterList[selected];
+	}
+
+	public void delete() {
+		if (letters.length() != 0) {
+			letters.substring(0, letters.length() - 1);
+		}
+	}
+
+	public String getLetters() {
+		return letters;
+	}
+}
